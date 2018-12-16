6d9101f72bc55f24d49397d83e399a4c1ac603ee
erdePL <erde@op.pl>
2018-12-16 18:26:48 +0100

    Initial commit

---
+++ src/main/java/RestfulMessageService/MyResource.java
@@ -0,0 +1,27 @@
+package RestfulMessageService;
+
+import RestfulMessageService.model.Message;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message getIt() {
+        return new Message(5,"Hello Message", "Author");
+    }
+}
